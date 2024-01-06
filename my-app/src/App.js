e7526d07dcc8f98e65aa063369b1890457721d92
AnnieKempf <annie.kempf@gmail.com>
2024-01-06 16:41:21 +0100

    Re-added my-app as a regular directory

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Business Performance Dashboard</h1>
+        <h4>Today's date and time</h4>
+      </header>
+
+      <main className="App-main">
+        <img src="/PowerBI_page-0001.jpg" className="dashboard"></img>
+      </main>
+    </div>
+  );
+}
+
+export default App;
